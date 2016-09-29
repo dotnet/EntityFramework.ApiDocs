40c471707ac6b7aa667a12b693ee378350de41f7
Tianqi Zhang <tianzh@microsoft.com>
2016-09-29 14:13:34 +0800

    update with latest build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 param(
-    [string] $docfxExeUrl
+    [string] $docfxExeUrl = "https://github.com/dotnet/docfx/releases/download/v2.5.4/docfx.zip"
 )
 $ErrorActionPreference = 'Stop'
 
@@ -7,52 +7,35 @@ $ErrorActionPreference = 'Stop'
 $scriptPath = $MyInvocation.MyCommand.Path
 $scriptHome = Split-Path $scriptPath
 $api_ref = Join-Path $scriptHome "api_ref"
-$tempFolder = "src"
+$tempFolder = Join-Path $scriptHome "src"
 md -Force $tempFolder
 
 Push-Location $scriptHome
 Push-Location $tempFolder
 
-if (Test-Path EntityFramework)
+$docfxZip = Join-Path $tempFolder "docfx.zip"
+$docfx = Join-Path $tempFolder "docfx"
+
+if (-Not (Test-Path $docfx\docfx.exe))
 {
-    Push-Location EntityFramework
-    & git pull
-    Pop-Location
+    # download docfx from github.com
+    Invoke-WebRequest $docfxExeUrl -OutFile $docfxZip
+
+    # unzip docfx.zip
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    function Unzip
+    {
+        param([string]$zipfile, [string]$outpath)
+
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+    }
+    Unzip $docfxZip $docfx
 }
-else
-{
-    & git clone https://github.com/aspnet/EntityFramework.git -q
-}
-
-$docfxZip = "docfx.zip"
-$docfx = "docfx"
-
-# download docfx from github.com
-Invoke-WebRequest $docfxExeUrl -OutFile $docfxZip
-
-# unzip docfx.zip
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
-
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
-
-Unzip $docfxZip $docfx
 
 # run docfx metadata to generate YAML metadata
-& .\$docfx\docfx.exe metadata $api_ref\docfx.json
+& $docfx\docfx.exe metadata $api_ref\docfx.json
 if($LASTEXITCODE -ne 0)
 {
     Pop-Location
     exit 1
 }
-
-# update to remote repo
-
-Pop-Location
-& git add .
-& git commit -m "CI Updates"
-& git push -u origin master
-Pop-Location
